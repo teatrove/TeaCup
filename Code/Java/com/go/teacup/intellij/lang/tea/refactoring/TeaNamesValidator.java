5692027b5bcac19e9f5f4252b700910c79406916
Brian R. Jackson <brian@jaxzin.com>
2011-03-07 14:25:39 -0500

    TEACUP-4: Attempting to rename parameter, claims invalid identifier

--- Code/Java/com/go/teacup/intellij/lang/tea/refactoring/TeaNamesValidator.java
+++ Code/Java/com/go/teacup/intellij/lang/tea/refactoring/TeaNamesValidator.java
@@ -5,6 +5,7 @@ import com.go.teacup.intellij.lang.tea.lexer.TeaTokenTypes;
 import com.intellij.lang.refactoring.NamesValidator;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.tree.IElementType;
 
 /**
  * User: JACKSBRR
@@ -13,14 +14,68 @@ import com.intellij.openapi.project.Project;
 public class TeaNamesValidator implements NamesValidator {
     private Lexer lexer = new TeaLexer();
 
-  public boolean isKeyword(String name, Project project) {
-    lexer.start(name, 0, name.length());
-    return TeaTokenTypes.KEYWORDS.contains( lexer.getTokenType() ) &&
-      lexer.getTokenEnd() == name.length();
-  }
+    public boolean isKeyword(String name, Project project) {
+        lexer.start(createDummyTemplate(name));
+        
+        return !skipDummyHeader()
+                && TeaTokenTypes.KEYWORDS.contains(lexer.getTokenType())
+                && (lexer.getTokenEnd()-lexer.getTokenStart()) == name.length();
+    }
 
-  public boolean isIdentifier(String name, Project project) {
-    lexer.start(name, 0, name.length());
-    return lexer.getTokenType() == TeaTokenTypes.IDENTIFIER &&
-      lexer.getTokenEnd() == name.length();
-  }}
+    public boolean isIdentifier(String name, Project project) {
+        lexer.start(createDummyTemplate(name));
+        return !skipDummyHeader()
+                && lexer.getTokenType() == TeaTokenTypes.IDENTIFIER
+                && (lexer.getTokenEnd()-lexer.getTokenStart()) == name.length();
+
+    }
+
+    private boolean skipDummyHeader() {
+        IElementType tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.LSCRIPT) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.SCRIPT_WHITE_SPACE) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.TEMPLATE_KEYWORD) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.SCRIPT_WHITE_SPACE) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.IDENTIFIER) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.LPAR) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.RPAR) {
+            return true;
+        }
+        lexer.advance();
+        tokenType = lexer.getTokenType();
+        if(tokenType != TeaTokenTypes.SCRIPT_WHITE_SPACE) {
+            return true;
+        }
+
+        lexer.advance();
+        return false;
+    }
+
+    private String createDummyTemplate(String body) {
+        return "<% template dummy()\n"+ body +"\n%>";
+    }
+}
